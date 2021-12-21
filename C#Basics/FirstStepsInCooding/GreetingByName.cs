3aef7cfbc4d51fd22b6086a5a37cc17656f8f01d
V.Grigorov <51731349+Vichondera@users.noreply.github.com>
2021-12-21 13:51:42 +0200

    Create GreetingByName.cs
    First Steps In Coding - Lab

---
+++ C#Basics/FirstStepsInCooding/GreetingByName.cs
@@ -0,0 +1,18 @@
+namespace greetings
+{
+    using System;
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = Console.ReadLine();
+            
+            Console.WriteLine($"Hello, {name}!");
+            
+            // Console.Write("Hello, ");
+            // Console.Write(name);
+            // Console.WriteLine("!");
+        }
+    }
+}
