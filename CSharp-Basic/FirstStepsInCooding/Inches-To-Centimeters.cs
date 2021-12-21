877e7cc195d7e2db7bdb62bf952b33e3a10dddf2
V.Grigorov <51731349+Vichondera@users.noreply.github.com>
2021-12-21 02:22:06 +0200

    Create Inches-To-Centimeters.cs
    LAB

---
+++ CSharp-Basic/FirstStepsInCooding/Inches-To-Centimeters.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace Inches2Centimeters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           double centimeters = double.Parse(Console.ReadLine());
+           double output = centimeters * 2.54;
+           Console.WriteLine(output);       
+            }
+    }
+}
