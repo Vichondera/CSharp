50a14d51ee8fe93992aa5851235810d3d08260e7
V.Grigorov <51731349+Vichondera@users.noreply.github.com>
2021-12-21 13:30:35 +0200

    Create RectangleArea.cs
    First Steps In Coding - Lab

---
+++ C#Basics/FirstStepsInCooding/RectangleArea.cs
@@ -0,0 +1,17 @@
+namespace RectangleArea
+{
+    using System;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sideA = int.Parse(Console.ReadLine());
+            int sideB = int.Parse(Console.ReadLine());
+
+            // int area = sideA * sideB;
+            Console.WriteLine(sideA * sideB);
+            // Console.WriteLine(area);
+        }
+    }
+}
