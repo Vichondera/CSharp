97dced666e7979fca3a847e6c18a8d8ae5694f24
V.Grigorov <51731349+Vichondera@users.noreply.github.com>
2021-12-21 14:42:23 +0200

    Exercise

---
+++ C#Basics/For Loop/Numbers Ending in 7.cs
@@ -0,0 +1,16 @@
+namespace EndingNum
+{
+    using System;
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 7; i <= 997; i++)
+                if (i % 10 == 7)
+                {
+                    Console.WriteLine(i);
+                }
+        }
+    }
+}
