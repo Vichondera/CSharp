d21db8af7972ca178bd211be79944e5876b15e77
V.Grigorov <51731349+Vichondera@users.noreply.github.com>
2021-12-21 15:08:34 +0200

    Lab

---
+++ C#Fundamentals/Arrays/Day of Week.cs
@@ -0,0 +1,34 @@
+namespace DayWeeks
+{
+    using System;
+    using System.Linq;
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            
+            string[] days = {
+                "Invalid day!",
+                "Monday",
+                "Tuesday",
+                "Wednesday",
+                "Thursday",
+                "Friday",
+                "Saturday",
+                "Sunday" 
+            };
+
+            if (n >= 1 && n <= 7)
+            {
+                Console.WriteLine(days[n]);
+            }
+            else
+            {
+                Console.WriteLine(days[0]);
+            }
+
+        }
+    }
+}
