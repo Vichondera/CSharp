b64da3dad8497b4b2de956780d5fb1c53d2f324f
V.Grigorov <51731349+Vichondera@users.noreply.github.com>
2021-12-27 23:55:43 +0200

    01. Encrypt, Sort and Print Array
    ⦁	Encrypt, Sort, and Print Array
    
    Write a program that reads a sequence of strings from the console. 
    Encrypt every string by summing:
    
    ⦁	The code of each vowel multiplied by the string length
    ⦁	The code of each consonant divided by the string length
    
    Sort the number sequence in ascending order and print it on the console.
    On the first line, you will always receive the number of strings you have to read.
    
    Examples
    Input                    Output                          Comments
    4
    Peter                   1032                           Peter = 1071
    Maria                  1071                           Maria = 1532
    Katya                  1168                           Katya = 1032
    Todor                 1532                            Todor = 1168

--- C#Fundamentals/Arrays/More Exercise/EncryptSortNPrint.cs
+++ C#Fundamentals/Arrays/More Exercise/EncryptSortNPrint.cs
@@ -1,1 +1,50 @@
+namespace Encrypt_Sort_and_Print_Array
+{
+    using System;
 
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numInputs = int.Parse(Console.ReadLine());
+            string[] sequenceStrings = new string[numInputs];
+
+            int[] valueString = new int[numInputs];
+
+            for (int i = 0; i < numInputs; i++)
+            {
+                sequenceStrings[i] = Console.ReadLine();
+                int sumVowels = 0;
+                int sumCons = 0;
+
+                foreach (char ch in sequenceStrings[i])
+                {
+                    if (ch == 'a' ||
+                        ch == 'e' || ch == 'i' ||
+                        ch == 'o' || ch == 'u' || ch == 'A' ||
+                        ch == 'E' || ch == 'I' || ch == 'O' || ch == 'U')
+                    {
+
+                        sumVowels += ((int)ch * sequenceStrings[i].Length);
+                    }
+                    else
+                    {
+                        sumCons += ((int)ch / sequenceStrings[i].Length);
+                    }
+                }
+
+                int stringSum = sumVowels + sumCons;
+
+                valueString[i] = stringSum;
+            }
+
+            Array.Sort(valueString);
+
+            foreach (int value in valueString)
+            {
+                Console.WriteLine(value);
+            }
+
+        }
+    }
+}
