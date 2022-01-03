a2de8f94f80b70505854721017d232f9b39b5ba2
V.Grigorov <51731349+Vichondera@users.noreply.github.com>
2022-01-03 19:11:46 +0200

    Word Synonyms
    ⦁	Word Synonyms
    Create a program, which keeps a dictionary with synonyms. The key of the dictionary will be the word. The value will be a list of all the synonyms of that word. You will be given a number n – the count of the words. After each word, you will be given a synonym, so the count of lines you have to read from the console is 2 * n. You will be receiving a word and a synonym each on a separate line like this:
    ⦁	{word}
    ⦁	{synonym}
    If you get the same word twice, just add the new synonym to the list. 
    Print the words in the following format:
    "{word} - {synonym1, synonym2… synonymN}"

---
+++ C#Fundamentals/Associative Arrays/Lab/WordSynonims.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace WordSynonims
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, List<string>> synonims = new Dictionary<string, List<string>>();
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                string word = Console.ReadLine();
+                string synonim = Console.ReadLine();
+
+                if (synonims.ContainsKey(word))
+                {
+                    synonims[word].Add(synonim);
+                }
+                else
+                {
+                    synonims.Add(word, new List<string>() { synonim });
+                }
+            }
+
+            foreach (var item in synonims)
+            {
+                Console.WriteLine($"{item.Key} - {string.Join(", ", item.Value)}");
+            }
+        
+        }
+
+    }
+}
