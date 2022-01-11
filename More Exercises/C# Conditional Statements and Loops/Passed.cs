6fcd40afb0cab773b2313a2b2f2921795b6d3b18
V.Grigorov <51731349+Vichondera@users.noreply.github.com>
2022-01-11 15:37:31 +0200

    01. Passed - Lab
    1. Passed
    Write a program, which takes as an input a grade 
    and prints “Passed!” if the grade is equal or more than 3.00.
    
    Input
    The input comes as a single floating-point number.
    
    Output
    The output is either "Passed!" if the grade is equal or more than 3.00, otherwise you should print nothing.
    
    Examples
    Input	 Output		Input	Output
    5.32    	Passed!		2.34	       (no output)

---
+++ More Exercises/C# Conditional Statements and Loops/Passed.cs
@@ -0,0 +1,18 @@
+namespace Passed
+{
+    using System;
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double grade = double.Parse(Console.ReadLine());
+
+            if(grade > 3.00)
+            {
+                Console.WriteLine("Passed!");
+            }
+
+        }
+    }
+}
