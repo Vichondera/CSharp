9ea19f16e6ea6b408f72ff8d0725c00118d7ccf6
V.Grigorov <51731349+Vichondera@users.noreply.github.com>
2022-01-16 00:23:30 +0200

    Create Strings and Objects.cs
    Declare two string variables and assign them with “Hello” and “World”. Declare an object variable and assign it with the concatenation of the first two variables (mind adding an interval between). Declare a third string variable and initialize it with the value of the object variable (you should perform type casting).

---
+++ More Exercises/Data Types and Variables/Strings and Objects.cs
@@ -0,0 +1,17 @@
+namespace Strings_and_Objects
+{
+    using System;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string a = Console.ReadLine();
+            string b = Console.ReadLine();
+
+            string toObj = a + " " + b;
+
+            Console.WriteLine(toObj);
+        }
+    }
+}
