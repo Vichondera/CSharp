b2099307cdb115597b8242c27db0d1f529b76d02
V.Grigorov <51731349+Vichondera@users.noreply.github.com>
2022-01-17 01:01:08 +0200

    01. Greeting - Lab
    ⦁	Greeting
    Write a program, which greets the user by their name, which it reads from the console.

---
+++ C#Fund-More Exercises/Intro and Basic Syntax/Greeting.cs
@@ -0,0 +1,14 @@
+namespace Greeting
+{
+    using System;
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = Console.ReadLine();
+
+            Console.WriteLine("Hello, {0}!", name);
+        }
+    }
+}
