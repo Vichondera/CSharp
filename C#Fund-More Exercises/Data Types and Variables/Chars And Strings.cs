d75088dcc5089645ed33d0221ccbee470830818b
V.Grigorov <51731349+Vichondera@users.noreply.github.com>
2022-01-17 01:11:58 +0200

    03. Practice Chars And Strings
    Create a program that assigns character and string values to variables. 
    Be sure that each value is stored in the correct variable.
     Finally, you need to print all variables to the console.

---
+++ C#Fund-More Exercises/Data Types and Variables/Chars And Strings.cs
@@ -0,0 +1,21 @@
+using System;
+
+namespace Characters_and_Strings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Software University
+            Console.WriteLine(Console.ReadLine());
+            // B
+            Console.WriteLine(char.Parse(Console.ReadLine()));
+            // y
+            Console.WriteLine(char.Parse(Console.ReadLine()));
+            // e
+            Console.WriteLine(char.Parse(Console.ReadLine()));
+            // I love programming
+            Console.WriteLine(Console.ReadLine());
+        }
+    }
+}
