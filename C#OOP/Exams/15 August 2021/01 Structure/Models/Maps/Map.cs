e78a51b7d367ff80667f3f89abdd9f588af9ccf7
V.Grigorov <51731349+Vichondera@users.noreply.github.com>
2022-04-16 01:51:24 +0300

    Create Map.cs

---
+++ C#OOP/Exams/15 August 2021/01 Structure/Models/Maps/Map.cs
@@ -0,0 +1,14 @@
+namespace CarRacing.Models.Maps
+{
+    using System;
+    using CarRacing.Models.Maps.Contracts;
+    using CarRacing.Models.Racers.Contracts;
+
+    public class Map : IMap
+    {
+        public string StartRace(IRacer racerOne, IRacer racerTwo)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
