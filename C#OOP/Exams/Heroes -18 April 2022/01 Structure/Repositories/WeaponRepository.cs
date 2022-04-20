402820ecf83c313566129d15a0a7b3b0c2825f69
V.Grigorov <51731349+Vichondera@users.noreply.github.com>
2022-04-20 15:21:22 +0300

    Create WeaponRepository.cs

---
+++ C#OOP/Exams/Heroes -18 April 2022/01 Structure/Repositories/WeaponRepository.cs
@@ -0,0 +1,28 @@
+using Heroes.Models.Contracts;
+using Heroes.Repositories.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Heroes.Repositories
+{
+    public class WeaponRepository : IRepository<IWeapon>
+    {
+        public IReadOnlyCollection<IWeapon> Models => throw new NotImplementedException();
+
+        public void Add(IWeapon model)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IWeapon FindByName(string name)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Remove(IWeapon model)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
