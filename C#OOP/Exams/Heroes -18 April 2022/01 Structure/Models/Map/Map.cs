a00afc13be639dec07e1fa2b4e318363534c5255
V.Grigorov <51731349+Vichondera@users.noreply.github.com>
2022-04-20 15:27:33 +0300

    Create Map.cs

---
+++ C#OOP/Exams/Heroes -18 April 2022/01 Structure/Models/Map/Map.cs
@@ -0,0 +1,16 @@
+using Heroes.Models.Contracts;
+
+namespace Heroes.Models.Map
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    public class Map : IMap
+    {
+        public string Fight(ICollection<IHero> players)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
