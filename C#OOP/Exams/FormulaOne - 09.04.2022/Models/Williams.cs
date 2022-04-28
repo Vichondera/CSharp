15397f47e392c39be3060fb4b06b027dd407c368
V.Grigorov <51731349+Vichondera@users.noreply.github.com>
2022-04-29 00:21:55 +0300

    Create Williams.cs

---
+++ C#OOP/Exams/FormulaOne - 09.04.2022/Models/Williams.cs
@@ -0,0 +1,10 @@
+namespace Formula1.Models
+{
+    public class Williams : FormulaOneCar
+    {
+        public Williams(string model, int horsepower, double engineDisplacement) 
+            : base(model, horsepower, engineDisplacement)
+        {
+        }
+    }
+}
