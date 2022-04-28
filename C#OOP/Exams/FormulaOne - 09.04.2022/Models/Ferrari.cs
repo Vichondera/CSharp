41dc9fcbfd632866613eec9d3d505f0be59aaf36
V.Grigorov <51731349+Vichondera@users.noreply.github.com>
2022-04-29 00:20:47 +0300

    Create Ferrari.cs

---
+++ C#OOP/Exams/FormulaOne - 09.04.2022/Models/Ferrari.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace Formula1.Models
+{
+    public class Ferrari : FormulaOneCar
+    {
+        public Ferrari(string model, int horsepower, double engineDisplacement) 
+            : base(model, horsepower, engineDisplacement)
+        {
+        }
+    }
+}
