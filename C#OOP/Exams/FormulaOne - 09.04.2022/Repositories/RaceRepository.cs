e36dbbd0baa3ea46522f19a071ea88e168d93906
V.Grigorov <51731349+Vichondera@users.noreply.github.com>
2022-04-29 00:19:16 +0300

    Create RaceRepository.cs

---
+++ C#OOP/Exams/FormulaOne - 09.04.2022/Repositories/RaceRepository.cs
@@ -0,0 +1,27 @@
+using Formula1.Models.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Formula1.Repositories.Contracts
+{
+    public class RaceRepository : IRepository<IRace>
+    {
+        public IReadOnlyCollection<IRace> Models => throw new NotImplementedException();
+
+        public void Add(IRace model)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IRace FindByName(string name)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Remove(IRace model)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
