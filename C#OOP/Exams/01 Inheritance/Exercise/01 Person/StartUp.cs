9762223a2d65ca7caf1fb2f31fa2b0ae7ddc74e5
V.Grigorov <51731349+Vichondera@users.noreply.github.com>
2022-08-21 15:55:51 +0300

    Create StartUp.cs

---
+++ C#OOP/Exams/01 Inheritance/Exercise/01 Person/StartUp.cs
@@ -0,0 +1,19 @@
+namespace Person
+{
+    using System;
+
+    using Person.Models;
+
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            string name = Console.ReadLine();
+            int age = int.Parse(Console.ReadLine());
+
+            Child child = new Child(name, age);
+            Console.WriteLine(child);
+
+        }
+    }
+}
