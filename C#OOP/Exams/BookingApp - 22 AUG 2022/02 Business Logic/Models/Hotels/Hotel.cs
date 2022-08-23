cf4850543580c8c3a5a5d072929d1155c5db31a8
V.Grigorov <51731349+Vichondera@users.noreply.github.com>
2022-08-23 20:33:20 +0300

    Add files via upload

---
+++ C#OOP/Exams/BookingApp - 22 AUG 2022/02 Business Logic/Models/Hotels/Hotel.cs
@@ -0,0 +1,24 @@
+﻿using BookingApp.Models.Bookings.Contracts;
+using BookingApp.Models.Hotels.Contacts;
+using BookingApp.Models.Rooms.Contracts;
+using BookingApp.Repositories.Contracts;
+
+namespace BookingApp.Models.Hotels
+{
+    public class Hotel : IHotel
+    {
+        public Hotel(string fullName, int category)
+        {
+
+        }
+
+        public string FullName => throw new System.NotImplementedException();
+
+        public int Category => throw new System.NotImplementedException();
+
+        public double Turnover => throw new System.NotImplementedException();
+
+        public IRepository<IRoom> Rooms { get => throw new System.NotImplementedException(); set => throw new System.NotImplementedException(); }
+        public IRepository<IBooking> Bookings { get => throw new System.NotImplementedException(); set => throw new System.NotImplementedException(); }
+    }
+}
---
+++ C#OOP/Exams/BookingApp - 22 AUG 2022/02 Business Logic/Repositories/HotelRepository.cs
@@ -0,0 +1,29 @@
+﻿using BookingApp.Models.Hotels.Contacts;
+using BookingApp.Repositories.Contracts;
+using System.Collections.Generic;
+
+namespace BookingApp.Repositories
+{
+    public class HotelRepository : IRepository<IHotel>
+    {
+        public HotelRepository()
+        {
+
+        }
+
+        public void AddNew(IHotel model)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public IReadOnlyCollection<IHotel> All()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public IHotel Select(string criteria)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
