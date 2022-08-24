17ecc4fa77bcf191d9372aec8323687474dfd83d
V.Grigorov <51731349+Vichondera@users.noreply.github.com>
2022-08-24 22:19:32 +0300

    Update Booking.cs

--- C#OOP/Exams/BookingApp - 22 AUG 2022/02 Business Logic/Models/Bookings/Booking.cs
+++ C#OOP/Exams/BookingApp - 22 AUG 2022/02 Business Logic/Models/Bookings/Booking.cs
@@ -5,24 +5,74 @@
     
     public class Booking : IBooking
     {
+        private int residenceDuration;
+        private int adultsCount;
+        private int childrenCount;
+        private int bookingNumber = 1;
+
         public Booking(IRoom room, int residenceDuration, int adultsCount, int childrenCount, int bookingNumber)
         {
-
+            this.Room = room;
+            this.ResidenceDuration = residenceDuration;
+            this.AdultsCount = adultsCount;
+            this.ChildrenCount = childrenCount;
+            this.bookingNumber = bookingNumber;
         }
 
-        public IRoom Room => throw new System.NotImplementedException();
+        public IRoom Room { get; set; }
 
-        public int ResidenceDuration => throw new System.NotImplementedException();
+        public int ResidenceDuration
+        {
+            get => residenceDuration;
+            protected set
+            {
+                if (value < 1)
+                {
+                    throw new ArgumentException("Duration cannot be negative or zero!");
+                }
+                residenceDuration = value;
+            }
+        }
 
-        public int AdultsCount => throw new System.NotImplementedException();
+        public int AdultsCount
+        {
+            get => adultsCount;
+            protected set
+            {
+                if (value < 1)
+                {
+                    throw new ArgumentException("Adults count cannot be negative or zero!");
+                }
+                adultsCount = value;
+            }
+        }
 
-        public int ChildrenCount => throw new System.NotImplementedException();
+        public int ChildrenCount
+        {
+            get => childrenCount;
+            protected set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentException("Children count cannot be negative!");
+                }
+                childrenCount = value;
+            }
+        }
 
-        public int BookingNumber => throw new System.NotImplementedException();
+        public int BookingNumber  => this.bookingNumber;
 
         public string BookingSummary()
         {
-            throw new System.NotImplementedException();
+            var sb = new StringBuilder();
+
+            sb.AppendLine($"Booking number: {this.BookingNumber}");
+            sb.AppendLine($"Room type: {nameof(this.Room)}");
+            sb.AppendLine($"Adults: {this.AdultsCount} Children: {this.ChildrenCount}");
+            sb.AppendLine($"Total amount paid: {TotalPaid():F2} $");
+            return sb.ToString().TrimEnd();
         }
+
+        private double TotalPaid() => Math.Round(this.Room.PricePerNight * this.residenceDuration, 2);
     }
 }
