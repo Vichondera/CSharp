3e9af3ab8484f3ebff5ece4821a7d2d49a335eab
V.Grigorov <51731349+Vichondera@users.noreply.github.com>
2023-12-14 13:47:28 +0200

    Create Product.cs

---
+++ C#Advanced/Exams/Retake Exam - 13 Dec 2023/03 Groceries Management/Product.cs
@@ -0,0 +1,17 @@
+namespace GroceriesManagement
+{
+    public class Product
+    {
+        public Product(string name,double price)
+        {
+            Name = name;
+            Price = price;
+        }
+
+        public string Name { get; set; }
+
+        public double Price { get; set; }
+
+        public override string ToString() => $"{Name}: {Price:F2}$";
+    }
+}
