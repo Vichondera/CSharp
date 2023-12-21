428145f5da2602dbd8b4a4ea08d63f421e56e72d
V.Grigorov <51731349+Vichondera@users.noreply.github.com>
2023-12-21 17:11:05 +0200

    Create StartUp.cs

---
+++ C#OOP/Exams/Nautical Catch Challenge - 09 DEC 2023/StartUp.cs
@@ -0,0 +1,14 @@
+namespace NauticalCatchChallenge
+{
+    using NauticalCatchChallenge.Core;
+    using NauticalCatchChallenge.Core.Contracts;
+
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            IEngine engine = new Engine();
+            engine.Run();
+        }
+    }
+}
