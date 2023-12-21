be4117629631452138ee32b94d6e016ef81fe6c3
V.Grigorov <51731349+Vichondera@users.noreply.github.com>
2023-12-21 17:14:06 +0200

    Create ReefFish.cs

---
+++ C#OOP/Exams/Nautical Catch Challenge - 09 DEC 2023/Models/ReefFish.cs
@@ -0,0 +1,10 @@
+namespace NauticalCatchChallenge.Models
+{
+    public class ReefFish : Fish
+    {
+        public ReefFish(string name, double points)
+            : base(name, points, 30)
+        {
+        }
+    }
+}
