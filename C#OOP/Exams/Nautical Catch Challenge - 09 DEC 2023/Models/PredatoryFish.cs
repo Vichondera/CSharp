944fae2ccd6c1b404cae8f764d77746a9bfeb544
V.Grigorov <51731349+Vichondera@users.noreply.github.com>
2023-12-21 17:13:51 +0200

    Create PredatoryFish.cs

---
+++ C#OOP/Exams/Nautical Catch Challenge - 09 DEC 2023/Models/PredatoryFish.cs
@@ -0,0 +1,10 @@
+namespace NauticalCatchChallenge.Models
+{
+    public class PredatoryFish : Fish
+    {
+        public PredatoryFish(string name, double points)
+            : base(name, points, 60)
+        {
+        }
+    }
+}
