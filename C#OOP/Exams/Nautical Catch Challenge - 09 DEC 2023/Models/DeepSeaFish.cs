8491ca2f0c97ccaa36f67d0c03449199354532f8
V.Grigorov <51731349+Vichondera@users.noreply.github.com>
2023-12-21 17:12:14 +0200

    Create DeepSeaFish.cs

---
+++ C#OOP/Exams/Nautical Catch Challenge - 09 DEC 2023/Models/DeepSeaFish.cs
@@ -0,0 +1,10 @@
+namespace NauticalCatchChallenge.Models
+{
+    public class DeepSeaFish : Fish
+    {
+        public DeepSeaFish(string name, double points)
+            : base(name, points, 180)
+        {
+        }
+    }
+}
