14732969030a905192b7892bf449855c6371cc5c
V.Grigorov <51731349+Vichondera@users.noreply.github.com>
2023-12-22 23:45:57 +0200

    Create DiverRepository.cs

---
+++ C#OOP/Exams/Nautical Catch Challenge - 09 DEC 2023/Repositories/DiverRepository.cs
@@ -0,0 +1,25 @@
+namespace NauticalCatchChallenge.Repositories
+{
+    using NauticalCatchChallenge.Models.Contracts;
+    using NauticalCatchChallenge.Repositories.Contracts;
+
+    public class DiverRepository : IRepository<IDiver>
+    {
+        private  List<IDiver> models;
+
+        public DiverRepository()
+        {
+            models = new List<IDiver>();
+        }
+
+        public IReadOnlyCollection<IDiver> Models => models.AsReadOnly();
+
+        public void AddModel(IDiver model) => models.Add(model);
+
+        public IDiver GetModel(string name)
+        {
+            return models.FirstOrDefault(x => x.Name == name);
+
+        }
+    }
+}
